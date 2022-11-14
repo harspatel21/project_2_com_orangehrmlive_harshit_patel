c2864ac95769f8c76732762e3dc0bb8403694b38
harspatel21 <harspatel@gmail.com>
2022-11-14 23:02:24 +0000

    Week 11 Selenium Homework Harshit Patel

---
+++ src/com_orangehrmlive/FireFoxComOrangeHrmLive.java
@@ -0,0 +1,50 @@
+package com_orangehrmlive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FireFoxComOrangeHrmLive {
+
+
+    public static void main(String[] args) {
+        //create baseUrl variable
+        String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+        //Setting up key and copy value from drivers file.
+        System.setProperty("webdriver.firefox.driver", "drivers/geckodriver.exe");
+        // Creating an object for Firefox driver
+        WebDriver driver = new FirefoxDriver();
+        //launching the url.
+        driver.get(baseUrl);
+        //Maximize window size
+        driver.manage().window().maximize();
+        //implicit time given to driver for 20 seconds.
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Getting and printing a title of the page.
+        String title = driver.getTitle();
+        System.out.println("Page title is : " + title);
+
+        //Printing current Url
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        //printing page source
+        System.out.println("Page Source code : " + driver.getPageSource());
+
+        //finding and email field element
+        WebElement emailField = driver.findElement(By.name("username"));
+        // sending a data to email field element
+        emailField.sendKeys("Admin");
+
+        // finding password field element
+        WebElement passwordField = driver.findElement(By.name("password"));
+        // Sending a data to password field element
+        passwordField.sendKeys("admin123");
+
+        //closing the window.
+        driver.quit();
+    }
+}
